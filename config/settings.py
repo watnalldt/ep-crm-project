1aadaebccd2d4179c3d55d719d1fea2defc41a5f
watnalldt <davidaturner56@yahoo.com>
2022-11-28 07:34:23 +0000

    Configure redis env variable

--- config/settings.py
+++ config/settings.py
@@ -201,7 +201,7 @@ RECIPIENT_ADDRESS = env("RECIPIENT_ADDRESS")
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": env("REDIS_URL"),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         },
